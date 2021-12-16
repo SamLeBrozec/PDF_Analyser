d621db2b102f6b617b261b0479915fe712672dc3
“[samuel <[samuel.lebrozec@gmail.com]>
2021-12-16 11:51:11 +0100

    A commented version

--- word_count.py
+++ word_count.py
@@ -3,6 +3,12 @@
 """
 Created on Thu Dec  9 14:46:47 2021
 
+Simple pdf analyser in order to count occurence of words in a job offer
+The punctuations character are removed
+The most link words are removed 
+save a sorted dictionnary in a csv file
+
+
 @author: samuel
 """
 
@@ -17,9 +23,24 @@ import sys
 
 
 def convert_pdf_to_txt(path):
+    """
+    Convert pdf file in str variable
+    
+    Parameters
+    ---------- 
+    path : str
+        path to the pdf file to convert
+    
+    Returns
+    -------
+    txt : str
+        contains the text inculde in the pdf file
+        
+    """
+    
     rsrcmgr = PDFResourceManager()
     retstr = StringIO()
-    codec = 'utf-8'
+    # codec = 'utf-8'
     laparams = LAParams()
     # device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
     device = TextConverter(rsrcmgr, retstr,  laparams=laparams)
@@ -41,20 +62,35 @@ def convert_pdf_to_txt(path):
     return text
 
 
-def clean_text(text):
+def clean_text(text, punctList = [',',';',':','.','!','/','\n','(',')',"’",'-','_',"'",'"']):
+    """
+    replace punctuation chrarcters from of a text by the space ' ' character
     
+    Parameters
+    ---------- 
+    text : str
+        text to clean 
     
+    punctList : list
+        list of charchter to be removed from the text 
     
-    for ponctuation in [',',';',':','.','!','/','\n','(',')',"’",'-','_',"'",'"']:
+    Returns
+    -------
+    outText : str
+        cleaned text
+        
+    """
+        
+    for ponctuation in punctList:
         text = text.replace(ponctuation," ")
     
     text = ''.join(i for i in text if not i.isdigit())
     text = text.split()
     text  = [''.join(i for i in t if i.isalpha()) for t in text]
     
-     
+    outText = text 
     
-    return text
+    return outText
 
 def count(L):
     d = dict()
@@ -72,21 +108,21 @@ def count(L):
 def sort(x):
     return dict(sorted(x.items(), key=lambda item: item[1],reverse = True))
 
-def removemotliaison(dic):
-    for liaison in ['de','des','les','la','le','au','sur','dans','ce','cette','et','pour','que','qui','à','du','l','d',
-                    'ou','notre','en','un','une','est','vous','votre','plus','ses','son','ces','avec','aux','vos',
-                    'il','elle','on','nos','êtes','par','lors','pas']:
+def removemotliaison(dic, wordList = ['de','des','les','la','le','au','sur','dans','ce','cette','et','pour','que','qui','à','du','l','d',
+                'ou','notre','en','un','une','est','vous','votre','plus','ses','son','ces','avec','aux','vos',
+                'il','elle','on','nos','êtes','par','lors','pas']):
+    
+    
+    
+    for liaison in wordList :
         if liaison in dic:
             dic.pop(liaison)
     return dic
 
 
 
-# print(sys.argv)
 file = sys.argv[1]
-# '/home/samuel/Documents/Samuel/CV/GENVIA/Oct21_IngenieurModelisationStack.pdf'
 fileout = file.split('.pdf')[0]+'.csv'
-# file ='/home/samuel/Documents/Samuel/CV/GENVIA/CV_LE_BROZEC.pdf'
 
 text = convert_pdf_to_txt(file)
 textList = clean_text(text)
